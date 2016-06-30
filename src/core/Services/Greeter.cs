66280b8d1dfae2b1b75367fd94647e9fe2b2678e
Martin Wåger <martin.w@comaround.se>
2016-06-30 23:02:41 +0200

    injection and services

---
+++ src/core/Services/Greeter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace core.services
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+    public class Greeter : IGreeter
+    {
+        private readonly string _greeter;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeter = configuration["greeting"];
+        }
+
+        public string GetGreeting()
+        {
+            return _greeter;
+        }
+    }
+}
--- src/core/Startup.cs
+++ src/core/Startup.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
+using core.services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,14 +11,25 @@ namespace core
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup()
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        private IConfiguration Configuration { get; set; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient(provider => Configuration);
+            services.AddTransient<IGreeter, Greeter>();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -30,7 +40,8 @@ namespace core
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                var greeting = greeter.GetGreeting();
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
