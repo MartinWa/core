30247d278cc27ecbe0e69fe4079da8d494141226
Martin Wåger <martin.w@comaround.se>
2016-07-02 22:20:59 +0200

    attribute routing

---
+++ src/core/Controller/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace core.Controller
+{
+    [Route("info/about/[action]")]
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "phonenr";
+        }
+
+        public string Country()
+        {
+            return "SW";
+        }
+    }
+}
