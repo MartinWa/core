baa96cc73c6837d1389f0287e8cdcffe7b52a7cc
Martin Wåger <martin.w@comaround.se>
2016-07-03 20:50:24 +0200

    EF Migrations

--- src/core/Entities/CoreDbContext.cs
+++ src/core/Entities/CoreDbContext.cs
@@ -4,6 +4,7 @@ namespace core.Entities
 {
     public class CoreDbContext : DbContext
     {
+        public CoreDbContext(DbContextOptions<CoreDbContext> options) : base(options) { }
         public DbSet<Resturant> Resturants { get; set; }
     }
 }
---
+++ src/core/Migrations/CoreDbContextModelSnapshot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using core.Entities;
+
+namespace core.Migrations
+{
+    [DbContext(typeof(CoreDbContext))]
+    partial class CoreDbContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("core.Entities.Resturant", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("Cuisine");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Resturants");
+                });
+        }
+    }
+}
